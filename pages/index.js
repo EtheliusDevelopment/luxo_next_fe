1150777dde1021d030373cb2e9443ff60357f599
EtheliusDevelopment <etheliusdev@gmail.com>
2021-06-15 10:28:09 +0200

    Vercel Test3

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
-import Image from 'next/image'
+
+import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -11,18 +12,7 @@ export default function Home() {
     <br/>
     <a href="/home">Home</a>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+    <Footer/>
     </div>
   )
 }
