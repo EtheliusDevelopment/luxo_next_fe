1169f8e7b97aff40147e4f31c45e28c8233c15d8
EtheliusDevelopment <etheliusdev@gmail.com>
2021-06-15 13:30:52 +0200

    i18n init

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,56 @@ import Head from 'next/head'
 import Footer from '../components/Footer'
 import styles from '../styles/Home.module.css'
 
+// configurazione i18n
+import { useRouter } from 'next/router';
+import {en, ru, it} from '../translation'
+
 export default function Home() {
+
+// configurazione i18n
+const router = useRouter();
+const { locale }= router;
+
+const t = locale === "it" ? it : en;
+
+const handleLanguageToogle = () =>{
+  switch (locale ) {
+
+    case "it":
+      router.push("/", "/", {
+        locale: "en"
+      })
+    break;
+
+    case "en":
+      router.push("/", "/", {
+        locale: "it"
+      })
+
+    break;
+
+    case "ru":
+      router.push("/", "/", {
+        locale: "ru"
+      })
+
+    break;
+  }
+}
+
+// fine configurazione i18n    
   return (
     <div className={styles.container}>
     
     <h1>Luxo Tour Operator</h1>
     <br/>
+    <h3>Language Test</h3>
+
+      <p onClick={handleLanguageToogle}>
+      {locale}
+      </p>
+    
+    <h1 style={{color: 'green', border:"2px dashed salmon", padding:"2em"}}>{t.linguaggio}</h1>
     <br/>
     <a href="/home">Home</a>
 
---
+++ translation.js
@@ -0,0 +1,13 @@
+export const it = {
+    linguaggio : "linguaggio"
+}
+
+
+export const en = {
+    linguaggio : "language"
+}
+
+export const ru = {
+    linguaggio : "язык"
+}
+
